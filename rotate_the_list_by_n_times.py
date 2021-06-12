53047373de58bac7e3c6e248cf26edfdd9640414
Anithasadhanala <84316640+Anithasadhanala@users.noreply.github.com>
2021-06-12 11:28:36 +0530

    Update rotate_the_list_by_n_times.py

--- rotate_the_list_by_n_times.py
+++ rotate_the_list_by_n_times.py
@@ -1,11 +1,16 @@
 def rotate_the_list(n,data,rotate):
     
-    if(rotate%n!=0):            #If the no,of items is the multiple of 
-        for i in range(rotate): # the rotate then no need to rotate as
-            k=data[0]           # we get the same data..
-            data.remove(k)
-            data.append(k)
-
+    if(rotate%n!=0):                                        #if the rotate is multiple of n then the same data
+       if(rotate>n):                          
+            for i in range(rotate%n):                       # if the rotate is > n then ,no need to ratate for those many times
+                k=data[0]                 
+                data.remove(k)
+                data.append(k)
+       else:
+            for i in range(rotate):                         # if the rotate is < n then, rotating those many times.....
+                k=data[0]           
+                data.remove(k)
+                data.append(k)
 
 
 n=int(input())
