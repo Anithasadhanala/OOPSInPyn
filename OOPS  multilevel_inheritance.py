f4d891667d75b53d149f1c90121fe797e368961b
Anithasadhanala <84316640+Anithasadhanala@users.noreply.github.com>
2021-09-10 16:57:39 +0530

    Add files via upload

---
+++ OOPS  multilevel_inheritance.py
@@ -0,0 +1,33 @@
+class Family:                                   #Parent class
+    me="Anitha"            
+    def __init__(self,father,mother,son):
+        self.father=father
+        self.mother=mother   
+        self.son=son
+    def member(self):
+        print(Family.me)     
+        print(self.father)    
+
+        
+class Superfamily(Family):                      #subclass of Family class and parent to Marvelfamily subclass
+    
+    def __init__(self,grand_father,grand_mother,father,mother,son):
+        self.grand_father=grand_father
+        self.grand_mother=grand_mother
+        super().__init__(father,mother,son)
+
+
+class Marvelfamily(Superfamily):                 #sub class of Superfamily
+
+    def __init__(self,uncle,anty,grand_father,grand_mother,father,mother,son):
+        self.uncle=uncle
+        self.anty=anty
+        super().__init__(grand_father,grand_mother,father,mother,son)
+
+main=Marvelfamily("honey","Bunny","padma","satyanarayana","krishna murthy","Rama devi","hari")
+
+
+print(main.father)                              #can access var of Family , Superfamily , marvelfamily also
+
+
+
